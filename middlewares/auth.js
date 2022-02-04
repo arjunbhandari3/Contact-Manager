cfc9ffb0ab1655464886cfdae7e2698dd7cf5747
Arjun Bhandari <arjunbhandari3773@gmail.com>
2022-02-04 10:45:57 +0545

    refactored middlewares

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -4,9 +4,8 @@ const jwt = require("jsonwebtoken");
 const auth = async (req, res, next) => {
   try {
     const token = req.header("Authorization").replace("Bearer ", "");
-    if (!token) {
-      return res.status(401).json({ message: "Unauthorized." });
-    }
+    if (!token) return res.status(401).json({ message: "Unauthorized." });
+
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
     const user = await User.findById(decoded.id);
--- middlewares/validator.js
+++ middlewares/validator.js
@@ -2,10 +2,13 @@ const { validationResult } = require("express-validator");
 
 exports.validator = async (req, res, next) => {
   const errors = validationResult(req);
-  if (errors.isEmpty()) return next();
 
-  const resultErrors = [];
-  errors.array().map((err) => resultErrors.push({ [err.param]: err.msg }));
-  const errorObject = Object.assign({}, ...resultErrors);
-  return res.status(422).json(errorObject);
+  if (!errors.isEmpty()) {
+    const resultErrors = [];
+    resultErrors.push({ success: false });
+    errors.array().map((err) => resultErrors.push({ [err.param]: err.msg }));
+    const errorObject = Object.assign({}, ...resultErrors);
+    return res.status(422).json(errorObject);
+  }
+  next();
 };
