80472d435c737000650db569b5e8363a11fd70df
Arjun Bhandari <arjunbhandari3773@gmail.com>
2022-02-08 01:25:36 +0545

    getUser route updated

--- middlewares/validator.js
+++ middlewares/validator.js
@@ -4,11 +4,16 @@ exports.validator = async (req, res, next) => {
   const errors = validationResult(req);
 
   if (!errors.isEmpty()) {
-    const resultErrors = [];
-    resultErrors.push({ success: false });
-    errors.array().map((err) => resultErrors.push({ [err.param]: err.msg }));
-    const errorObject = Object.assign({}, ...resultErrors);
-    return res.status(422).json(errorObject);
+    // const resultErrors = [];
+    // resultErrors.push({ success: false });
+    // errors.array().map((err) => resultErrors.push({ [err.param]: err.msg }));
+    // const errorObject = Object.assign({}, ...resultErrors);
+    // return res.status(422).json(errorObject);
+    let error = errors.array()[0];
+    return res.status(422).json({
+      success: false,
+      message: error.msg,
+    });
   }
   next();
 };
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 const router = require("express").Router();
 const authRoutes = require("./auth_route");
+const userRoutes = require("./user_route");
 const contactRoutes = require("./contact_route");
 
-router.use("/api/auth", authRoutes);
-router.use("/api/contacts", contactRoutes);
+router.use("/api/auth/", authRoutes);
+router.use("/api/user/", userRoutes);
+router.use("/api/contacts/", contactRoutes);
 
 module.exports = router;
